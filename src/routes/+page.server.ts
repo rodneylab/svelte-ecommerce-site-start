f0cebc72dbd351cdecd2eca63bad9f6b2636717c
Rodney Lab <codesign@rodneylab.com>
2022-10-01 05:21:07 +0100

    fix: 💫 update error handling

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -5,6 +5,10 @@ export const load: PageServerLoad = async function () {
 	try {
 		return {};
 	} catch (err: unknown) {
+		const httpError = err as { status: number; message: string };
+		if (httpError.status && httpError.message) {
+			throw error(httpError.status, httpError.message);
+		}
 		throw error(500, err as string);
 	}
 };
