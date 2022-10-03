72a495ee9a87f1e5f72b6dddee21f24dff0f8691
Rodney Lab <codesign@rodneylab.com>
2022-10-03 04:23:56 +0100

    fix: 💫 add Svelte error file

---
+++ src/routes/+error.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	const {
+		status,
+		error,
+	} = $page;
+
+	const {message = ''}  = error ?? {};
+	const title = `${status}: ${message}`;
+</script>
+
+<svelte:head>
+	<title>{title}</title>
+</svelte:head>
+<h1>{title}</h1>
+<p>
+	{message}&hellip; <a aria-label="Return to home page" href="/">Go&nbsp;home?</a>
+</p>
+
+<style>
+	a {
+		color: var(--colour-dark);
+	}
+</style>
