3d95e6cea811035c4371baf166ea2399793cba16
Rodney Lab <codesign@rodneylab.com>
2022-10-03 23:47:11 +0100

    fix: 🐝  add config file

---
+++ src/lib/configuration/website.ts
@@ -0,0 +1,17 @@
+import {
+	PUBLIC_SITE_URL,
+} from '$env/static/public';
+
+const website = {
+	author: 'Rodney Johnson',
+	ogLanguage: 'en_GB',
+	siteLanguage: 'en-GB',
+	siteTitle: 'SvelteKit eCommerce Store',
+	siteShortTitle: 'SvelteKit Store',
+	description: 'Svelte eCommerce site.',
+	siteUrl: PUBLIC_SITE_URL,
+	backgroundColor: '#1b4079',
+	themeColor: '#d62828',
+};
+
+export { website as default };
